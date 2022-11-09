4588fccb0e86663161a0e2d9192e1dfb57cb7391
Christopher Persaud <christopherp543@nycstudents.net>
2022-11-09 12:20:24 -0500

    c

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+       WhileLoopFun fun = new WhileLoopFun();
+        System.out.println("-- test isPrime --");
+        System.out.println(fun.isPrime(2));
+        System.out.println(fun.isPrime(13));
+        System.out.println(fun.isPrime(773));
+        System.out.println(fun.isPrime(9857));
+        System.out.println(fun.isPrime(101879));
+        System.out.println(fun.isPrime(1));
+        System.out.println(fun.isPrime(14));
+        System.out.println(fun.isPrime(30));
+        System.out.println(fun.isPrime(771));
+        System.out.println(fun.isPrime(93243));
+
+
+    }
+}
